08b73bd3a40fd1c980f9656d84a750ae25f9a01a
ssplast <ssplast@i.ua>
2017-04-12 20:45:08 +0300

    cheerio
    cheerio

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var compression  = require('compression');
 
 var request = require('request');
 var htmlParser = require('html-parser');
+var cheerio = require("cheerio");
 require('./telegram');
 
 
@@ -64,42 +65,35 @@ io.on('connection', function (socket) {
     });
 });
 
+
+
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
     /*  Seconds: 0-59
-        Minutes: 0-59
-        Hours: 0-23
-        Day of Month: 1-31
-        Months: 0-11
-        Day of Week: 0-6    */
-    cronTime: '*/5 */1 6-20 1-31 0-11 0-6',
+     Minutes: 0-59
+     Hours: 0-23
+     Day of Month: 1-31
+     Months: 0-11
+     Day of Week: 0-6    */
+    cronTime: '*/5 */1 0-23 1-31 0-11 0-6',
     onTick: function() {
         console.log(moment().format());
         request('https://kharkov.obmenka.ua/', function (error, response, body) {
+
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            //console.log('body:', body); // Print the HTML for the Google homepage.
-
-            /*var sanitized = htmlParser.sanitize(body, {
-                elements: [ 'script' ],
-                type: [ 'application/ld+json' ],
-                //attributes: [ 'type' ],
-                comments: true
+            //console.log(body); // Print the HTML for the Google homepage.
+            //http://37.187.109.16/ua
+            $ = cheerio.load(body);
+            var curcur = {};
+            $('li[class=" direction"]').slice(0, 3).each(function(i, elem) {
+                $(this).find('span[class="currency"]').text().trim();
+                $(this).find('span[class="buy"]').text().trim();
+                $(this).find('span[class="sell"]').text().trim();
+                console.log($(this).find('span[class="currency"]').text().trim());
+                console.log($(this).find('span[class="buy"]').text().trim());
+                console.log($(this).find('span[class="sell"]').text().trim());
             });
-
-            console.log(sanitized);
-            */
-            htmlParser.parse(body, {
-                openElement: function(name) { console.log('open: %s', name); },
-                closeOpenedElement: function(name, token, unary) { console.log('token: %s, unary: %s', token, unary); },
-                closeElement: function(name) { console.log('close: %s', name); },
-                comment: function(value) { console.log('comment: %s', value); },
-                cdata: function(value) { console.log('cdata: %s', value); },
-                attribute: function(name, value) { console.log('attribute: %s=%s', name, value); },
-                docType: function(value) { console.log('doctype: %s', value); },
-                text: function(value) { console.log('text: %s', value); }
-            });
-
         });
     },
     start: false,
